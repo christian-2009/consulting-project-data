e7c15b94f22b8bd4e7e375caac743dfeb8484e47
christian-2009 <c-sophocleous@hotmail.com>
2022-05-28 17:01:37 +0100

    began fetching data and adding pagination

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,14 +1,27 @@
+import axios from 'axios'
+import React, {useState, useEffect} from 'react';
+
+interface ProjectInterface {
+    id: string;
+    clientId: string;
+    employeeIds : [];
+    contract: {
+        startDate: string;
+        endDate: string;
+        size: string;
+    }
+}
+
 export default function MainContent(): JSX.Element {
+    const [projects, setProjects] = useState<ProjectInterface[]>([])
   const employeeId = 4;
   const clientId = 3;
   return (
     <>
-      <a href={`http://localhost:3000/ + employees/ + ${employeeId}  `}>
-        Employee
-      </a>
+      <a href={`http://localhost:3000/ + employees/ + ${employeeId}  `}>Employee</a>
       <br />
       <a href={`http://localhost:3000/ + clients/ + ${clientId}  `}>Client</a>
       <h1>Main</h1>
     </>
-  );
+  ); 
 }
