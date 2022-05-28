07640ff9dda1d454408076c1c44e19ed03f620de
christian-2009 <c-sophocleous@hotmail.com>
2022-05-28 15:34:03 +0100

    sorted react router and created basic components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import { greet } from "./utils/greet";
+
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import MainContent from "./components/MainContent";
+import Employee from "./components/Employee";
+import ClientInfo from "./components/ClientInfo";
 
 function App(): JSX.Element {
-  return <h1>{greet("World")}</h1>;
+  return (
+  <Router>
+    <>
+    <Routes>
+      <Route path='/' element={<MainContent />} />
+      <Route path = '/employees/:employeeId' element = {<Employee />}/>
+      <Route path = '/clients/:clientId' element = {<ClientInfo />} />
+      </Routes>
+    </>
+  </Router>
+  )
 }
 
 export default App;
