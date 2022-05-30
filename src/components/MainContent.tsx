023f0b49a2f606d2128d61a0b31db94ea6ccb81f
christian-2009 <c-sophocleous@hotmail.com>
2022-05-30 16:41:12 +0100

    filter functionality added

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -51,8 +51,8 @@ export default function MainContent(): JSX.Element {
   const [state, dispatch] = useReducer(reducer, {projects: [], clients: [], employees: []})
   const [searchTerm, setSearchTerm] = useState<string>('')
   const [projectsWithClientsAndEmployees, setProjectsWithClientsAndEmployees] =useState<ProjectInterface[]>([])
-  const [filteredProjects, setFilteredProjects] = useState<ProjectInterface[]>([])
-
+  
+  //getting client, employee and project data and combining them
   useEffect(() => {
      const fetchData = async () => {
         const projectData = await axios.get('https://consulting-projects.academy-faculty.repl.co/api/projects')
@@ -70,20 +70,20 @@ export default function MainContent(): JSX.Element {
     
   },[])
 
-  const handleSearch = () => {
-    setFilteredProjects(projectsWithClientsAndEmployees.filter((project) => 
-      typeof project.client !== 'undefined' && project.client.toLowerCase().includes(searchTerm.toLowerCase())
-    ))
-  }
+ //filter the array of projects based on client and employees who worked on it
+  const filtered = projectsWithClientsAndEmployees.filter((project) => 
+    (typeof project.client !== 'undefined' && project.client.toLowerCase().includes(searchTerm.toLowerCase())) ||
+    checkEmployeeOnProject(project.employeeIds)
+  )
 
-  const handleEnter = (event: React.KeyboardEvent) => {
-    if(event.key === 'Enter'){
-      handleSearch()
+  function checkEmployeeOnProject(array: string[]){
+    for(const employee of array){
+      if (employee.toLowerCase().includes(searchTerm.toLowerCase())){
+        return true
     }
-  }
-
-  console.log('this is all projects with employee names and client names', projectsWithClientsAndEmployees)
-  console.log('this is projects', state.projects, 'this is clients', state.clients, 'this is employees', state.employees)
+  } 
+}
+  
   const employeeId = 4;
   const clientId = 3;
   return (
@@ -91,17 +91,15 @@ export default function MainContent(): JSX.Element {
       <a href={`http://localhost:3000/ + employees/ + ${employeeId}  `}>Employee</a>
       <br />
       <a href={`http://localhost:3000/ + clients/ + ${clientId}  `}>Client</a>
-      <h1>Main</h1>
+      <h1>Dashboard</h1>
 
       <input
         onChange={(e) => {setSearchTerm(e.target.value)}}
-        onKeyDown={(e) => handleEnter(e)}
         type='text'
         placeholder='search projects...'
         value={searchTerm}
       ></input>
-      {filteredProjects.length === 0 ? <IndividualProjects projects={projectsWithClientsAndEmployees} /> : 
-      <IndividualProjects projects={filteredProjects} /> }
+      <IndividualProjects projects={filtered} /> 
       
     </>
   ); 
